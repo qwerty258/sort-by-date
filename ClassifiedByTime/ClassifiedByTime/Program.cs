68b99aa150b4a36e02932c5e55db107d851fee7b
qwerty258 <zyf26256@gmail.com>
2016-07-06 22:52:02 +0800

    A little program to sort files A program to put same last write time (modified time) into a dir.

---
+++ ClassifiedByTime/ClassifiedByTime/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassifiedByTime
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string rootDir = Directory.GetCurrentDirectory();
+            Console.Write("Root Dir: ");
+            Console.WriteLine(rootDir);
+            DirectoryInfo rootDirInfo = new DirectoryInfo(rootDir);
+            foreach (FileInfo fileInfoTemp in rootDirInfo.GetFiles())
+            {
+                if (fileInfoTemp.Name.Contains("ClassifiedByTime"))
+                    continue;
+                Console.WriteLine(fileInfoTemp.Name);
+                DateTime LastWriteDateTime = fileInfoTemp.LastWriteTime;
+
+                string newPathName = LastWriteDateTime.ToString("yyyy_MM_dd");
+
+                newPathName = rootDir + "\\" + newPathName;
+
+                if (!Directory.Exists(newPathName))
+                    Directory.CreateDirectory(newPathName);
+
+                string oldPathName = rootDir + "\\" + fileInfoTemp.Name;
+                newPathName = newPathName + "\\" + fileInfoTemp.Name;
+
+                File.Move(oldPathName, newPathName);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
